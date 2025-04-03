51a597454b449c547187a6db845beb15260f03fb
richie-rich90454 <zhiyu.jiang90454-bisz@basischina.com>
2025-04-03 15:33:02 +0800

    153203

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("#brief").click(function(){
         $("#who-i-am").slideToggle(400, function(){
             $(this).is(":visible")?
-                $("#breif").text("Hide"):
+                $("#brief").text("Hide"):
                 $("#brief").text("Do you want to know me?");
         });
     });
