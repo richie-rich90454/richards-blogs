28f4f2f2d71dba08262025f874ec1a268141416e
richie-rich90454 <zhiyu.jiang90454-bisz@basischina.com>
2025-04-03 14:15:32 +0800

    141503

--- public/script.js
+++ public/script.js
@@ -6,4 +6,21 @@ $(document).ready(function(){
                 $("#brief").text("Do you want to know me?");
         });
     });
+    function updateLocalTime(){
+        let now=new Date();
+        $("#time-h").text(formatTime(now.getHours())+":");
+        $("#time-m").text(formatTime(now.getMinutes())+":");
+        $("#time-s").text(formatTime(now.getSeconds()));
+        let offset=now.getTimezoneOffset();
+        $("#time-zone").text(
+            offset>0?
+                ` (UTC-${Math.abs(offset/60)})`:
+                ` (UTC+${Math.abs(offset/60)})`
+        );
+    }
+    function formatTime(time){
+        return time<10?"0"+time:time;
+    }
+    updateLocalTime();
+    setInterval(updateLocalTime, 1000);
 });
