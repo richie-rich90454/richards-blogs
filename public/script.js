a77743c751d846fda4ed0af712f554800214305c
richie-rich90454 <zhiyu.jiang90454-bisz@basischina.com>
2025-04-03 13:58:12 +0800

    135803

--- public/script.js
+++ public/script.js
@@ -1,4 +1,9 @@
-let breifButton=document.getElementById("brief");
-breifButton.addEventListener("click", function(){
-    document.getElementById("who-i-am").style.display="block";
+$(document).ready(function(){
+    $("#brief").click(function(){
+        $("#who-i-am").slideToggle(400, function(){
+            $(this).is(":visible")?
+                $("#breif").text("Hide"):
+                $("#brief").text("Do you want to know me?");
+        });
+    });
 });
